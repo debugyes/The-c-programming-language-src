fbc0b718aa85351c9b50ae6487f6c88450462f6e
debugyes <1075292937@qq.com>
2020-05-21 22:02:03 +0800

    update

---
+++ src/chapter1/字符数组.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#define MAXLINE 1000
+
+int getLine(char line[], int maxline);
+void copy(char to[], char from[]);
+
+int main(void)
+{
+	int max = 0;
+	char line[MAXLINE];
+	char longest[MAXLINE];
+
+
+	return 0;
+}
+
+int getLine(char line[], int maxline)
+{
+	char c;
+	int i, c;
+	for ()
+	return 0;
+}
