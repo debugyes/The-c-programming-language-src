0edf3c64f013a4c235bb5252b039887c914948d0
Debug yes! <40919466+debugyes@users.noreply.github.com>
2020-05-26 11:22:24 +0800

    Update 字符数组.c

--- src/chapter1/字符数组.c
+++ src/chapter1/字符数组.c
@@ -6,9 +6,21 @@ void copy(char to[], char from[]);
 
 int main(void)
 {
+	int len = 0;
 	int max = 0;
 	char line[MAXLINE];
 	char longest[MAXLINE];
+
+	while(len = getline(line, MAXLINE) > 0)
+	{
+		if(len > max)
+		{
+			max = len;
+			copy(longest, line);
+		}
+	}
+	if(max > 0)
+		printf("%s", longest);
 
 
 	return 0;
@@ -18,6 +30,29 @@ int getLine(char line[], int maxline)
 {
 	char c;
 	int i, c;
-	for ()
-	return 0;
+	for (i = 0; i < lim-1 && (c = getchar()) != EOF && c != '\n'; ++i)
+	{
+		s[i] = c;
+	}
+
+	if(c == '\n')
+	{
+		s[i] = c;
+		++i;
+	}
+
+	s[i] = '\0';
+	return i;
+}
+
+//把char from赋值进char to
+void copy(char to[], char from[])
+{
+	int i;
+
+	i = 0;
+	while((to[i] = from [i]) != '\0')
+	{
+		i++;
+	}
 }
